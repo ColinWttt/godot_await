29ff85f360670d103d822ba962e34fea874a1274
CoPsyche <cognitivepsyche@gmail.com>
2025-03-23 17:15:21 +0800

    itest: #![allow(dead_code)]

--- itest/rust/src/framework/mod.rs
+++ itest/rust/src/framework/mod.rs
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
+#![allow(dead_code)]
 use godot::classes::{Engine, Node, Os};
 use godot::obj::Gd;
 use godot::sys;
