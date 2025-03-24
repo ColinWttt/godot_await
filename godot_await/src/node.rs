1b1e381f5dd119f465ccdf1d7573f49d01bcb5cf
CoPsyche <cognitivepsyche@gmail.com>
2025-03-24 13:33:35 +0800

    Rename: node fn

--- godot_await/src/node.rs
+++ godot_await/src/node.rs
@@ -11,12 +11,12 @@ where
     // child_order_changed
 
     /// Emitted when the node enters the tree.
-    fn node_tree_entered(&self) -> SignalFuture<()>;
+    fn tree_entered(&self) -> SignalFuture<()>;
     /// Emitted after the node exits the tree and is no longer active.
-    fn node_tree_exited(&self) -> SignalFuture<()>;
+    fn tree_exited(&self) -> SignalFuture<()>;
     /// Emitted when the node is just about to exit the tree. The node is still valid.
     /// As such, this is the right place for de-initialization (or a "destructor", if you will).
-    fn node_tree_exiting(&self) -> SignalFuture<()>;
+    fn tree_exiting(&self) -> SignalFuture<()>;
 }
 
 impl<T> NodeExt<T> for Gd<T>
@@ -24,17 +24,17 @@ where
     T: Inherits<Node>,
 {
     #[inline]
-    fn node_tree_entered(&self) -> SignalFuture<()> {
+    fn tree_entered(&self) -> SignalFuture<()> {
         Signal::from_object_signal(self, "tree_entered").to_future::<()>()
     }
 
     #[inline]
-    fn node_tree_exited(&self) -> SignalFuture<()> {
+    fn tree_exited(&self) -> SignalFuture<()> {
         Signal::from_object_signal(self, "tree_exited").to_future::<()>()
     }
 
     #[inline]
-    fn node_tree_exiting(&self) -> SignalFuture<()> {
+    fn tree_exiting(&self) -> SignalFuture<()> {
         Signal::from_object_signal(self, "tree_exiting").to_future::<()>()
     }
 }
